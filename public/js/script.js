0360fbc3a0e26f6f47fa17c65291fbbb958ffe01
Saha <404110@cognizant.com>
2018-10-15 20:08:35 +0530

    Commit 1

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,111 @@
+var globalMovieList = [];
+
 function getMovies() {
 
+	let movieDataPromise = fetch('http://localhost:3000/movies', {
+		method: 'get'
+	}).then(response => response.json());
+
+	movieDataPromise
+		.then(parseListOfAllMovies)
+		.catch(parseError);
+
+	return movieDataPromise;
+}
+
+function parseError(error) {
+	console.log('Error occured');
+	console.log(error);
+}
+
+function parseListOfAllMovies(listOfMovies) {
+
+	if (Array.isArray(listOfMovies)) {
+
+		globalMovieList = [];
+		globalMovieList = listOfMovies;
+
+		listOfMovies.forEach(element => {
+
+			let innerContent = `
+				<li id='${element.id}'>
+					<span>${element.name}</span>
+					<button id='addMovieButton' type='button' class='btn btn-link' value='${element.id}' onclick="addFavourite('${element.id}')">
+						Add to Fav
+					</button>
+				</li>
+			`;
+
+			document.getElementById('moviesList').innerHTML += innerContent;
+		})
+	}
 }
 
 function getFavourites() {
 
+	let favDataPromise = fetch('http://localhost:3000/favourites', {
+		method: 'get'
+	}).then(response => response.json());
+
+	favDataPromise
+		.then(parseListOfAllFavourites)
+		.catch(parseError);
+
+	return favDataPromise;
+
 }
 
-function addFavourite() {
+function parseListOfAllFavourites(listOfFavs) {
 
+	document.getElementById('favouritesList').innerHTML = '';
+	if (Array.isArray(listOfFavs)) {
+		listOfFavs.forEach(element => {
+
+			let innerContent = `
+				<li id='${element.id}'>
+					<span>${element.name}</span>
+					<button id='removeMovieButton' type='button' class='btn btn-link' value='${element.id}'>Remove</button>
+				</li>
+			`;
+
+			document.getElementById('favouritesList').innerHTML += innerContent;
+		})
+	}
+}
+
+function addFavourite(movieID) {
+	//console.log(`Movie ID = ${movieID}`);
+
+	let selectedMovie;
+
+	if(movieID) {
+		selectedMovie = globalMovieList.find(element => {
+			//console.log(`Movie ID = ${element.id}`);
+			return element.id === movieID;
+		});
+	}
+
+	// console.log('selectedMovie : ');
+	// console.log(JSON.stringify(selectedMovie));
+
+	let favDataAddPromise = fetch('http://localhost:3000/favourites', {
+		method: 'POST',
+		body: JSON.stringify(selectedMovie),
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	}).then((res) => res.json())
+	.catch(function(error) {
+		console.log(`error in POST`);
+		console.log(`${error}`);
+	});
+
+	favDataAddPromise
+	.then(getFavourites)
+	.catch(parseError);
+
+	return favDataAddPromise;
 }
 
 module.exports = {
